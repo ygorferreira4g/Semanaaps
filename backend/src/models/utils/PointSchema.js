8a9623ecfe09ba524773b43801c21717ebf2695f
alvaroico <alvaro.r.p@hotmail.com>
2020-01-15 23:07:46 -0300

    Ajuste erro mongoose

---
+++ backend/src/models/utils/PointSchema.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+// ajuste erro de continuaçao
+mongoose.set('useCreateIndex', true);
+
+const PointSchema = new mongoose.Schema({
+    type:{
+        type: String,
+        enum: ['Point'],
+        required: true,
+    },
+    coordinates:{
+        type: [Number],
+        required: true,
+    },
+});
+
+module.exports = PointSchema;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,28 +7,10 @@
 
 // MongoDB (Não-relacional)
 const { Router } = require('express');
-const axios = require('axios'); 
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
 
 const routes = Router();
 
-routes.post('/devs', async (request,response) => {
-    const { github_username, techs } = request.body;
-
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`); 
-
-    const { name = login, avatar_url, bio } = apiResponse.data;
-
-  const techsArray = techs.split(',').map(tech => tech.trim());
-
-   const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray,
-   })
-    return response.json(dev); 
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
