661d8b0ce50537bbaf54bb42562ab7e9fa565391
alvaroico <alvaro.r.p@hotmail.com>
2020-01-15 23:12:12 -0300

    criar OK

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -4,8 +4,11 @@ const Dev = require('../models/Dev');
 module.exports = {
     async store(request,response) {
         const { github_username, techs, latitude, longitude } = request.body;
-    
-        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`); 
+
+        let dev = await Dev.findOne({ github_username});
+
+        if (!dev){
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`); 
     
         const { name = login, avatar_url, bio } = apiResponse.data;
     
@@ -16,7 +19,7 @@ module.exports = {
             coordinates: [longitude, latitude],
         }  
     
-       const dev = await Dev.create({
+       dev = await Dev.create({
             github_username,
             name,
             avatar_url,
@@ -24,6 +27,9 @@ module.exports = {
             techs: techsArray,
             location,
        })
+
+        }
+            
         return response.json(dev); 
     }
 };
