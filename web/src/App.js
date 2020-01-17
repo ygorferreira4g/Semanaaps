a82c09c934a30d301582b048d3c19cf314021e79
alvaroico <alvaro.r.p@hotmail.com>
2020-01-16 22:53:47 -0300

    Propiedade

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 // Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
-// Estado
-// Propiedade
+// Propiedade: Informaçoes que um componente PAI passa para o componeete FILHO
+// Estado: 
 
 import Header from './Header';
 
 function App() {
   return (
-    <Header />
+    <>
+    <Header title="Titulo 1" />
+    <Header title="Titulo 2" />
+    <Header title="Titulo 4" />
+    </>
   );
 }
 
