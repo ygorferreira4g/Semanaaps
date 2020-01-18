e87aecb496f7c035003120775155fdad268f0cd0
alvaroico <alvaro.r.p@hotmail.com>
2020-01-18 17:35:24 -0300

    app.use(cors());

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const Routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,8 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-us9xx.gcp.mongodb.n
     useUnifiedTopology: true
 });
 
+//app.use(cors({ origin: 'http://localhost:3000'}));
+app.use(cors());
 app.use(express.json());
 app.use(Routes);
 
