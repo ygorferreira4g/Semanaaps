7c003de90b07406d2c29c27f9954191323dfc3c9
alvaroico <alvaro.r.p@hotmail.com>
2020-01-19 13:09:20 -0300

    MAPS OK

--- mobile/App.js
+++ mobile/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+
 import Routes from './src/routes';
+
 
 export default function App() {
   return (
+    <>
+    <StatusBar barStyle="light-content" backgroudColor="#7d40e7"/>
     <Routes />
+    </>
   );
 }
 
--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View } from 'react-native';
+//import { View } from 'react-native';
+import MapView from 'react-native-maps';
 
 function Main(){
-    return <View />
+    return <MapView style={{ flex: 1}}/>
 }
 
 export default Main;
