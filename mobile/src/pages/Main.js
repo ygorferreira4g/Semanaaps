3d91dd66819987dd04f0d241d07006550a2beefc
alvaroico <alvaro.r.p@hotmail.com>
2020-01-20 22:18:25 -0300

    OK Localizaçáão

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,48 @@
-import React from 'react';
-//import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
+//import { useEffect } from 'react';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'; 
 
 function Main(){
-    return <MapView style={{ flex: 1}}/>
+    const [ currentRegion, setCurrentRegion ] = useState(null);
+
+    useEffect(() => {
+        async function loadInitialPossition(){
+            const { granted } = await requestPermissionsAsync();
+
+            if (granted){
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+                
+                const {latitude, longitude } = coords;
+
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
+                })
+                
+            }
+        }
+
+        loadInitialPossition();
+
+    }, []);
+
+    if (!currentRegion){
+        return null;
+    }
+
+    return <MapView initialRegion={ currentRegion } style={styles.map} />
 }
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    },
+})
 
 export default Main;
