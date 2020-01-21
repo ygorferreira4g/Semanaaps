a29d14987d50f0cb0c3725d9926b5f0b96da36b1
alvaroico <alvaro.r.p@hotmail.com>
2020-01-20 23:22:01 -0300

    Perfil OK  WebViw

--- mobile/src/pages/Profile.js
+++ mobile/src/pages/Profile.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-function Profile(){
-    return <View />
+function Profile({ navigation }) {
+    const githuUsername = navigation.getParam('github_username');
+
+    return <WebView style={{ flex: 1 }} source={{ uri: `http://github.com/${githuUsername}`}} />
 }
 
 export default Profile;
