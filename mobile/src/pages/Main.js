44eb6d7eb42c9d9820a7c66fb710b6882d6724df
alvaroico <alvaro.r.p@hotmail.com>
2020-01-20 22:51:23 -0300

    Clik Map OK

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import {StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
-//import { useEffect } from 'react';
+import {StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'; 
 
 function Main(){
@@ -36,13 +35,59 @@ function Main(){
         return null;
     }
 
-    return <MapView initialRegion={ currentRegion } style={styles.map} />
+    return (
+    <MapView initialRegion={ currentRegion } style={styles.map} >
+        <Marker coordinate={{ latitude: -12.5426564, longitude: -55.7125747 }} >
+        <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/59286021?v=4'}} ></Image>
+        
+        <Callout>
+            <View style={styles.callout}>
+                <Text  style={styles.devName} >Alvaro Ribeiro Pereira</Text>
+                <Text style={styles.devBioe} >orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a</Text>
+                <Text style={styles.devTechs} > Java, Android, C++</Text>
+
+            </View>
+
+        </Callout>
+        
+        </Marker>    
+    </MapView>
+    );
 }
 
 const styles = StyleSheet.create({
     map: {
         flex: 1
     },
+
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#fff'
+    },
+
+    callout: {
+        width: 260,
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+
+    devBioe: {
+        color: '#666',
+        marginTop: 5,
+    },
+
+    devTechs: {
+        marginTop: 5,
+    }
+   
+
+
 })
 
 export default Main;
